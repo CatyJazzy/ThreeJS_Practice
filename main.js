07bd3e002a9f17b3358bbe3db181d443d73b5a44
jihoo <ahrefhoo@naver.com>
2024-07-08 17:49:06 +0900

    feat: 튜토리얼1_세팅 초기화

--- main.js
+++ main.js
@@ -1,12 +1,59 @@
 import * as THREE from "three";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+class BasicWorldDemo {
+  constructor() {
+    this._Initialize();
+  }
+
+  _Initialize() {
+    /* three.js 기본 param들 설정 */
+    this._threejs = new THREE.WebGLRenderer();
+    this._threejs.shadowMap.enabled = true;
+    this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
+    this._threejs.setPixelRatio(window.devicePixelRatio);
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(this._thireejs.domElement);
+
+    window.addEventListener(
+      "resize",
+      () => {
+        this._OnWindowReSize();
+      },
+      false
+    );
+
+    /* 카메라 정의 정보 */
+    const fov = 60; // 시야각
+    const aspect = window.innerWidth / window.innerHeight; // 종횡비
+    const near = 1.0;
+    const far = 1000.0;
+    // 카메라 설정
+    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, fear);
+    this._camera.position.set(75, 20, 0);
+
+    /* Scene 설정 */
+    this._scene = new THREE.Scene();
+
+    /* 조명 설정 */
+    let light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(100, 100, 100);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true; // 동적 그림자 생성
+    light.shadow.bias = -0.01;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 1.0;
+    light.shadow.camera.far = 500;
+    light.shadow.camera.left = 200;
+    light.shadow.camera.right = -200;
+    light.shadow.camera.top = 200;
+    light.shadow.camera.bottom = -200;
+    this._scene.add(light);
+
+    light = new THREE.AmbientLight(0x404040);
+    this._scene.add(light);
+
+    this._RAF();
+  }
+}
